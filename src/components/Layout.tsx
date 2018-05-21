099c3bf7d79ee64d7eea79ebb90355a6ca4f4551
Fredrik Olsson <fr3gu77@gmail.com>
2018-05-21 10:27:13 +0200

    Header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
-import Header from "./common/Header"
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Header } from "./common/Header"
 import Home from "./home/Home";
 import About from "./about/About";
 import NoMatch from "./NoMatch";
@@ -11,14 +11,16 @@ export default class Layout extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <p>Header here...</p>
-                <HashRouter>
-                    <Switch>
-                        <Route name="home" path="/" exact={true} component={Home} />
-                        <Route name="about" path="/about" component={About} />
-                        <Route name="notfound" path="*" component={NoMatch} />
-                    </Switch>
-                </HashRouter>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Switch>
+                            <Route name="home" path="/" exact={true} component={Home} />
+                            <Route name="about" path="/about" component={About} />
+                            <Route name="notfound" path="*" component={NoMatch} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
             </div>
         );
     }
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,22 +1,12 @@
-import React, { Component } from "react"
+import React from "react"
 import "./Header.less"
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export default class Header extends Component {
-    render() {
-
-        return(
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a href="/" className="navbar-brand">
-                    <img src="images/logo.jpg" alt="logo" style={{ height: "60px" }}/>
-                </a>
-                <ul className="navbar-nav">
-                    <li className="nav-item"><Link className="nav-link" to="">Home</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/authors">Authors</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/courses">Courses</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-                </ul>
-            </nav>
-        );
-    }
+export const Header = () => {
+    return (
+        <nav>
+            <NavLink to="/" activeClassName="active" exact>Home</NavLink>&nbsp;|&nbsp;
+            <NavLink to="/about" activeClassName="active" exact>About</NavLink>
+        </nav>
+    )
 }
